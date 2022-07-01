fbb301f2ef740f13e46b13cbc02fdfe7cd136eac
Rudhra Koul <rudhra.koul@techpertitsolutions.com>
2022-07-02 00:59:10 +0530

    attribute selectors

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: 'app-servers',
+  selector: `[app-servers]`,
   template: `<server-component></server-component>
     <hr />
     <server-component></server-component> `,
